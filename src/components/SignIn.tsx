bb49947e34457fea89032987cec479b5feb1b69c
Krisliltech <mgbekwutechristian91@gmail.com>
2021-10-15 01:24:20 +0100

    updated

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -4,80 +4,80 @@ import { Button, TextField, Box } from "@mui/material";
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import GoogleIcon from '@mui/icons-material/Google';
 import axios from 'axios';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import dotenv from 'dotenv'
 import "@fontsource/poppins"
 dotenv.config();
 
+const useStyles = makeStyles({
+    bodys: {
+        backgroundColor: "whitesmoke",
+        width: "100%",
+        height: "85vh",
+    },
+    boxWrapper: {
+        position: "absolute",
+        height: '60vh',
+        width: '35%',
+        top: "30%",
+        left: "32%",
+        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .2)",
+        borderRadius: "10px"
+    },
+    boxs: {
+        marginTop: "9.5%",
+        marginLeft: "10%",
+        fontFamily: "poppins"
+    },
+    email: {
+        display: "flex",
+        width: "400px",
+        color: "white",
+        borderRadius: "10px",
+    },
+    btn: {
+        width: "400px",
+    },
+    headerWrapper: {
+        background: '#65c368',
+        height: '10vh',
+        padding: '30px 0 10px 25px'
+    },
+    headerText: {
+        color: 'white',
+        fontFamily: 'poppins',
+        letterSpacing: '1px',
+    },
+    formStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    formText: {
+        textAlign: 'center',
+        marginRight: '40px',
+        marginBottom: '20px',
+        fontFamily: 'poppins',
+        letterSpacing: '1px'
+    },
+    formIcons: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginRight: '40px',
+    },
+    fb: {
+        marginRight: '10px'
+    },
+    signup: {
+        color: 'green'
+    }
+});
 
 const SignInForm = () => {
     const [email, setemail] = useState("");
     const [password, setpassword] = useState("");
     const [showWarning, setShowWarning] = useState(false);
     const [warningMessage, setWarningMsg] = useState("")
-
-    const useStyles = makeStyles({
-        bodys: {
-            backgroundColor: "whitesmoke",
-            width: "100%",
-            height: "85vh",
-        },
-        boxWrapper: {
-            position: "absolute",
-            height: '60vh',
-            width: '35%',
-            top: "30%",
-            left: "32%",
-            boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .2)",
-            borderRadius: "10px"
-        },
-        boxs: {
-            marginTop: "9.5%",
-            marginLeft: "10%",
-            fontFamily: "poppins"
-        },
-        email: {
-            display: "flex",
-            width: "400px",
-            color: "white",
-            borderRadius: "10px",
-        },
-        btn: {
-            width: "400px",
-        },
-        headerWrapper: {
-            background: '#65c368',
-            height: '10vh',
-            padding: '30px 0 10px 25px'
-        },
-        headerText: {
-            color: 'white',
-            fontFamily: 'poppins',
-            letterSpacing: '1px',
-        },
-        formStyle: {
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        formText: {
-            textAlign: 'center',
-            marginRight: '40px',
-            marginBottom: '20px',
-            fontFamily: 'poppins',
-            letterSpacing: '1px'
-        },
-        formIcons: {
-            display: 'flex',
-            justifyContent: 'center',
-            marginRight: '40px',
-        },
-        fb: {
-            marginRight: '10px'
-        },
-        signup: {
-            color: 'green'
-        }
-    });
+    const history = useHistory()
 
     function checkLoginDetails(){
         if(!email && !password){
@@ -102,6 +102,7 @@ const SignInForm = () => {
             let result = null
             try{
                 result = await axios.post("https://notesxd.herokuapp.com/users/login", details)
+                history.push('/homepage')
             } catch (err:any) {
                 console.log(err, "wertyuio")
                 result = err.response;
